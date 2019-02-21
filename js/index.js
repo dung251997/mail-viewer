5373168e52c3b5205007eec2f102957c1da6efee
Diện Vô <dung251997@gmail.com>
2019-02-21 17:26:56 +0700

    finish

--- js/index.js
+++ js/index.js
@@ -1041,3 +1041,25 @@ jQuery(document).ready(function($) {
 })(jQuery, window, document);
 
 //# sourceMappingURL=jquery.nanoscroller.js.map
+/**********************************
+
+Orinal code from this article:
+http://www.barneyparker.com/world-simplest-html5-wysisyg-inline-editor/
+
+**********************************/
+
+$(function() {
+  $('#editControls a').click(function(e) {
+    switch($(this).data('role')) {
+      case 'h1':
+      case 'h2':
+      case 'p':
+        document.execCommand('formatBlock', false, $(this).data('role'));
+        break;
+      default:
+        document.execCommand($(this).data('role'), false, null);
+        break;
+    }
+    
+  });
+});
